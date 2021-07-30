97b9311ba1bb332b4907185c20cea61809e8f2af
Su <1535631290@qq.com>
2021-07-30 21:15:14 +0800

    deploy

---
+++ precache-manifest.e64ad63e278d99caf2072b35b4f03db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e863188593438f9316",
+    "url": "/morney-website/css/app.a8f9fa1d.css"
+  },
+  {
+    "revision": "e256ac796ba42024f6450f61a9f119b8",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "f8e863188593438f9316",
+    "url": "/morney-website/js/app.36c55920.js"
+  },
+  {
+    "revision": "8f695822be93565e5dbf",
+    "url": "/morney-website/js/chunk-vendors.d1e691ae.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
